df3310bf56e9665920853cd8543823be7d514ed9
Rogério Lino <rogeriolino@gmail.com>
2026-03-24 10:10:43 -0300

    chore: symfony 7.4 deprecations (#4)

--- src/DependencyInjection/NovosgaCustomersExtension.php
+++ src/DependencyInjection/NovosgaCustomersExtension.php
@@ -15,7 +15,7 @@ namespace Novosga\CustomersBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -26,7 +26,7 @@ class NovosgaCustomersExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
